93c05dc784ff822b41440170b1591e228c1d186a
nasum <nasu@scouty.co.jp>
2018-09-09 00:02:34 +0900

    add debug mode to kawazanyou

--- kawazanyou/kawazanyou.py
+++ kawazanyou/kawazanyou.py
@@ -7,6 +7,10 @@ parser.add_argument('-i', '--initial', type=int, help='Initial value')
 parser.add_argument('-r', '--rate', type=int, help='Annual interest rate')
 parser.add_argument('-s', '--saving', type=int, help="Annual saving")
 parser.add_argument('-t', '--term', type=int, help="term")
+parser.add_argument('-d', '--debug',
+                    required=False,
+                    action='store_true',
+                    help="debug mode")
 
 
 args = parser.parse_args()
@@ -15,17 +19,22 @@ initial = args.initial
 rate = args.rate
 saving = args.saving
 term = args.term
+debug = args.debug
 
-print('initial : {}'.format(initial))
-print('rate    : {} %'.format(rate))
-print('saving  : {}'.format(saving))
-print('term    : {} year'.format(term))
+
+if debug:
+    print('initial : {}'.format(initial))
+    print('rate    : {} %'.format(rate))
+    print('saving  : {}'.format(saving))
+    print('term    : {} year'.format(term))
 
 actual = initial
+
+print('year, actual, profit')
 
 for var in range(0, term):
     total_saving_amount = saving * 12
     actual += total_saving_amount
     profit = actual * (rate / 100)
     actual += profit
-    print('{} : {} ({})'.format(var, actual, profit))
+    print('{},{},{},'.format(var, actual, profit))
