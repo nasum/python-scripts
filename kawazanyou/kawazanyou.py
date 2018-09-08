8b2dba3ed517700b870b2be87ecae76b08f267a3
nasum <nasu@scouty.co.jp>
2018-09-08 23:50:02 +0900

    edit kawazanyou logic

--- kawazanyou/kawazanyou.py
+++ kawazanyou/kawazanyou.py
@@ -3,11 +3,29 @@ import argparse
 
 parser = argparse.ArgumentParser(description='kawazanyou')
 
-parser.add_argument('-i','--initial', type=int, help='Initial value')
-parser.add_argument('-r','--rate', type=int, help='Annual interest rate')
+parser.add_argument('-i', '--initial', type=int, help='Initial value')
+parser.add_argument('-r', '--rate', type=int, help='Annual interest rate')
 parser.add_argument('-s', '--saving', type=int, help="Annual saving")
+parser.add_argument('-t', '--term', type=int, help="term")
 
 
 args = parser.parse_args()
-if(args.display):
-  print(args.accumulate(args.initial))
+
+initial = args.initial
+rate = args.rate
+saving = args.saving
+term = args.term
+
+print('initial : {}'.format(initial))
+print('rate    : {} %'.format(rate))
+print('saving  : {}'.format(saving))
+print('term    : {} year'.format(term))
+
+actual = initial
+
+for var in range(0, term):
+    total_saving_amount = saving * 12
+    actual += total_saving_amount
+    profit = actual * (rate / 100)
+    actual += profit
+    print('{} : {} ({})'.format(var, actual, profit))
