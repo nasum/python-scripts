3695bad51ad56cf6dd76543386f2cdae63e1962e
nasum <nasu@scouty.co.jp>
2018-09-17 22:19:55 +0900

    add install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo 'install kawazanyou'
-ln kawazanyou/kawazanyou.py ~/bin/kawazanyou       
+ln kawazanyou/kawazanyou.py ~/bin/kawazanyou
+echo 'install show_image'
+ln show_image/show_image.py ~/bin/show_image
