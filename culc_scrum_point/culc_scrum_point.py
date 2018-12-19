91f8f89a08c0faf927a39dac929557d244f7addf
nasum <nasu@scouty.co.jp>
2018-12-19 17:26:59 +0900

    add culc scrum point command

---
+++ culc_scrum_point/culc_scrum_point.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import argparse
+
+parser = argparse.ArgumentParser(description='culc_scrum_point')
+
+parser.add_argument('-p', '--point',
+                    required=True,
+                    type=float,
+                    help='total point')
+parser.add_argument('-d', '--day',
+                    required=True,
+                    type=int,
+                    help='day number')
+parser.add_argument('-t', '--term',
+                    required=True,
+                    type=int,
+                    help='term number')
+parser.add_argument('-c', '--complete',
+                    required=True,
+                    type=float,
+                    help='complete point')
+
+
+
+args = parser.parse_args()
+
+point_number = args.point
+day = args.day
+complete_point = args.complete
+term = args.term
+
+point_by_day = point_number / term
+scheduled_point = point_by_day * day
+point_to_complete_by_day = (point_number - complete_point) / (term - day)
+
+print(f'総ポイント数　　　　　：{point_number}')
+print(f'実消化ポイント数　　　：{complete_point}')
+print(f'予定消化ポイント数　　：{scheduled_point}')
+print(f'遅れ　　　　　　　　　：{scheduled_point - complete_point}')
+print(f'１日あたりのポイント数：{point_to_complete_by_day}')
--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ echo 'install show_image'
 ln show_image/show_image.py ~/bin/show_image
 echo 'install add_image_ext'
 ln add_image_ext/add_image_ext.py ~/bin/add_image_ext
+echo 'install culc_scrum_point'
+ln culc_scrum_point/culc_scrum_point.py ~/bin/culc_scrum_point
